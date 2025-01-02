dedf5d5c0b68e78a2dd994fd6bd5677cbacc6bef
nev <nev@neverix.io>
2025-01-02 13:59:40 -0500

    Basic DSPy explanation and testing

--- sae_auto_interp/utils.py
+++ sae_auto_interp/utils.py
@@ -43,6 +43,6 @@ def load_tokenizer(model):
     """
 
     tokenizer = AutoTokenizer.from_pretrained(model, padding_side="left")
-    tokenizer._pad_token = tokenizer._eos_token
+    tokenizer._pad_token = tokenizer.eos_token
 
     return tokenizer
